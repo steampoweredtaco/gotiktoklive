c8ae73b6a3b8df3f4061dee10da3d65434a43d39
Steam Powered Taco <steampoweredtaco@gmail.com>
2024-09-21 16:01:06 -0600

    Fixup sending ack
    * Ack sends a payload equal to internalExt
    * Made internalExt binary as that fits with the payload of webframe that takes it, and I can't see it
     used elsewhere for anything string specific.
    * Continue processing messages when sending ack fails, any disconnect will happen later but might as well send all the messages to the client user first.

--- options.go
+++ options.go
@@ -9,8 +9,16 @@ func SigningApiKey(apiKey string) TikTokLiveOption {
 	}
 }
 
-// EnableExperimentalEvents enables experimental events that have not been figured out yet and the API for them
-// is not stable.  It may also induce additional logging that might be undesirable.
+// EnableExperimentalEvents enables experimental events that have not been
+// figured out yet and the API for them is not stable. It may also induce
+// additional logging that might be undesirable.
 func EnableExperimentalEvents(t *TikTok) {
 	t.enableExperimentalEvents = true
+}
+
+// EnableExtraWebCastDebug an unreasonable amount of debug for librarydevelopment
+// and troubleshooting. This option makes no gurantee of ever having the same
+// output and is only for development and triage purposes.
+func EnableExtraWebCastDebug(t *TikTok) {
+	t.enableExtraDebug = true
 }
